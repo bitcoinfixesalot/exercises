398ad921a3464db1b264635d0228b7459dd52917
Svetlan <s.atanasov@live.com>
2019-09-08 13:57:19 +0100

    clock exercise changes

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,8 +1,7 @@
 use std::fmt;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
-    pub hours: i32,
     pub minutes: i32,
 }
 
@@ -10,25 +9,19 @@ const MINUTES_PER_DAY: i32 = 24 * 60;
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut minutes = (hours * 60 + minutes) % MINUTES_PER_DAY;
-
-        if minutes < 0 {
-            minutes += MINUTES_PER_DAY;
-        }
-
+        let total_minutes = hours * 60 + minutes;
         Clock {
-            hours: minutes / 60,
-            minutes: minutes % 60,
+            minutes: ((total_minutes % MINUTES_PER_DAY) + MINUTES_PER_DAY) % MINUTES_PER_DAY,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock::new(self.hours, self.minutes + minutes)
+        Clock::new(0, self.minutes + minutes)
     }
 }

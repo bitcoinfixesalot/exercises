00c9dfa795f0f777852046d31aeec3ace0fd8521
Svetlan <s.atanasov@live.com>
2019-10-12 11:23:28 +0100

    optimizing anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,22 +2,22 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut result: HashSet<&'a str> = HashSet::new();
-    let word_vec = to_vec_char(word);
+
+    let word = word.to_lowercase();
+    let word_vec = to_vec_char(&word);
+
     for possible in possible_anagrams {
-        let possible_anagram_vec = to_vec_char(possible);
-        if word_vec == possible_anagram_vec && !is_same(word, possible) {
+        let possible_lower = possible.to_lowercase();
+        let possible_anagram_vec = to_vec_char(&possible_lower);
+        if word_vec == possible_anagram_vec && word != possible_lower {
             result.insert(possible);
         }
     }
     result
 }
 
-fn to_vec_char(word: &str) -> Vec<char> {
-    let mut vec: Vec<char> = word.to_lowercase().chars().collect();
+fn to_vec_char(word: &String) -> Vec<char> {
+    let mut vec: Vec<char> = word.chars().collect();
     vec.sort();
     vec
-}
-
-fn is_same(a: &str, b: &str) -> bool {
-    a.to_lowercase() == b.to_lowercase()
 }

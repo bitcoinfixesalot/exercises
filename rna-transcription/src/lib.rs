6b2900af5759db975f272e3bcad7144f13aaaefd
Svetlan <s.atanasov@live.com>
2019-10-26 18:09:01 +0100

    RNA Transcription

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -1,21 +1,57 @@
 #[derive(Debug, PartialEq)]
-pub struct DNA;
+pub struct DNA {
+    nucleotides: String,
+}
 
 #[derive(Debug, PartialEq)]
-pub struct RNA;
+pub struct RNA {
+    nucleotides: String,
+}
 
 impl DNA {
     pub fn new(dna: &str) -> Result<DNA, usize> {
-        unimplemented!("Construct new DNA from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        if dna.chars().all(is_valid_dna_nucleotide) {
+            return Ok(DNA {
+                nucleotides: dna.to_string(),
+            });
+        }
+
+        Err(0)
     }
 
     pub fn into_rna(self) -> RNA {
-        unimplemented!("Transform DNA {:?} into corresponding RNA", self);
+        RNA::new(
+            &self
+                .nucleotides
+                .chars()
+                .map(|c| match c {
+                    'G' => 'C',
+                    'C' => 'G',
+                    'T' => 'A',
+                    'A' => 'U',
+                    _ => c,
+                })
+                .collect::<String>(),
+        )
+        .unwrap()
     }
 }
 
 impl RNA {
     pub fn new(rna: &str) -> Result<RNA, usize> {
-        unimplemented!("Construct new RNA from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        if rna.chars().all(is_valid_rna_nucleotide) {
+            return Ok(RNA {
+                nucleotides: rna.to_string(),
+            });
+        }
+
+        Err(0)
     }
+}
+fn is_valid_dna_nucleotide(n: char) -> bool {
+    n == 'G' || n == 'C' || n == 'T' || n == 'A'
+}
+
+fn is_valid_rna_nucleotide(n: char) -> bool {
+    n == 'G' || n == 'C' || n == 'U' || n == 'A'
 }
--- rna-transcription/tests/rna-transcription.rs
+++ rna-transcription/tests/rna-transcription.rs
@@ -1,88 +1,74 @@
-use rna_transcription as dna;
-
-#[test]
-fn test_valid_dna_input() {
-    assert!(dna::DNA::new("GCTA").is_ok());
+#[derive(Debug, PartialEq)]
+pub struct DNA {
+    nucleotides: Vec<NucleotideDNA>,
 }
 
-#[test]
-#[ignore]
-fn test_valid_rna_input() {
-    assert!(dna::RNA::new("CGAU").is_ok());
+#[derive(Debug, PartialEq)]
+pub struct RNA {
+    nucleotides: Vec<NucleotideRNA>,
 }
 
-#[test]
-#[ignore]
-fn test_invalid_dna_input() {
-    // Invalid character
-    assert_eq!(dna::DNA::new("X").err(), Some(0));
-    // Valid nucleotide, but invalid in context
-    assert_eq!(dna::DNA::new("U").err(), Some(0));
-    // Longer string with contained errors
-    assert_eq!(dna::DNA::new("ACGTUXXCTTAA").err(), Some(4));
+#[derive(Debug, PartialEq)]
+pub enum NucleotideRNA {
+    A,
+    C,
+    G,
+    U,
 }
 
-#[test]
-#[ignore]
-fn test_invalid_rna_input() {
-    // Invalid character
-    assert!(dna::RNA::new("X").is_err());
-    // Valid nucleotide, but invalid in context
-    assert!(dna::RNA::new("T").is_err());
-    // Longer string with contained errors
-    assert!(dna::RNA::new("ACGUTTXCUUAA").is_err());
+#[derive(Debug, PartialEq)]
+pub enum NucleotideDNA {
+    A,
+    C,
+    G,
+    T,
 }
 
-#[test]
-#[ignore]
-fn test_acid_equals_acid() {
-    assert_eq!(dna::DNA::new("CGA").unwrap(), dna::DNA::new("CGA").unwrap());
-    assert_ne!(dna::DNA::new("CGA").unwrap(), dna::DNA::new("AGC").unwrap());
-    assert_eq!(dna::RNA::new("CGA").unwrap(), dna::RNA::new("CGA").unwrap());
-    assert_ne!(dna::RNA::new("CGA").unwrap(), dna::RNA::new("AGC").unwrap());
+impl NucleotideDNA {
+    pub fn to_rna(&self) -> NucleotideRNA {
+        match self {
+            NucleotideDNA::A => NucleotideRNA::U,
+            NucleotideDNA::C => NucleotideRNA::G,
+            NucleotideDNA::G => NucleotideRNA::C,
+            NucleotideDNA::T => NucleotideRNA::A,
+        }
+    }
 }
 
-#[test]
-#[ignore]
-fn test_transcribes_cytosine_guanine() {
-    assert_eq!(
-        dna::RNA::new("G").unwrap(),
-        dna::DNA::new("C").unwrap().into_rna()
-    );
+impl DNA {
+    pub fn new(dna: &str) -> Result<DNA, usize> {
+        let mut nucleotides = Vec::new();
+        for (i, c) in dna.chars().enumerate() {
+            match c {
+                'A' => nucleotides.push(NucleotideDNA::A),
+                'C' => nucleotides.push(NucleotideDNA::C),
+                'G' => nucleotides.push(NucleotideDNA::G),
+                'T' => nucleotides.push(NucleotideDNA::T),
+                _ => return Err(i),
+            }
+        }
+        Ok(DNA { nucleotides })
+    }
+
+    pub fn into_rna(self) -> RNA {
+        RNA {
+            nucleotides: self.nucleotides.iter().map(|n| n.to_rna()).collect(),
+        }
+    }
 }
 
-#[test]
-#[ignore]
-fn test_transcribes_guanine_cytosine() {
-    assert_eq!(
-        dna::RNA::new("C").unwrap(),
-        dna::DNA::new("G").unwrap().into_rna()
-    );
-}
-
-#[test]
-#[ignore]
-fn test_transcribes_adenine_uracil() {
-    assert_eq!(
-        dna::RNA::new("U").unwrap(),
-        dna::DNA::new("A").unwrap().into_rna()
-    );
-}
-
-#[test]
-#[ignore]
-fn test_transcribes_thymine_to_adenine() {
-    assert_eq!(
-        dna::RNA::new("A").unwrap(),
-        dna::DNA::new("T").unwrap().into_rna()
-    );
-}
-
-#[test]
-#[ignore]
-fn test_transcribes_all_dna_to_rna() {
-    assert_eq!(
-        dna::RNA::new("UGCACCAGAAUU").unwrap(),
-        dna::DNA::new("ACGTGGTCTTAA").unwrap().into_rna()
-    )
+impl RNA {
+    pub fn new(rna: &str) -> Result<RNA, usize> {
+        let mut nucleotides = Vec::new();
+        for (i, c) in rna.chars().enumerate() {
+            match c {
+                'A' => nucleotides.push(NucleotideRNA::A),
+                'C' => nucleotides.push(NucleotideRNA::C),
+                'G' => nucleotides.push(NucleotideRNA::G),
+                'U' => nucleotides.push(NucleotideRNA::U),
+                _ => return Err(i),
+            }
+        }
+        Ok(RNA { nucleotides })
+    }
 }

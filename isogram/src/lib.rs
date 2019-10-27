6a78ea6ff777b8181f1af29552ea7bdee2675f17
Svetlan <s.atanasov@live.com>
2019-10-27 23:44:24 +0000

    isogram

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -1,3 +1,17 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut existing = HashSet::new();
+    for c in candidate
+        .to_lowercase()
+        .chars()
+        .filter(|a| a.is_alphabetic())
+    {
+        if existing.contains(&c) {
+            return false;
+        } else {
+            existing.insert(c);
+        }
+    }
+    true
 }

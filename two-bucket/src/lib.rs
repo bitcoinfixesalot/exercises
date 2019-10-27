7b3e337198201d6cb402e293a39e2db6c2c50c60
Svetlan <s.atanasov@live.com>
2019-10-27 23:01:33 +0000

    two-buckets

--- two-bucket/src/lib.rs
+++ two-bucket/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum Bucket {
     One,
@@ -17,12 +20,53 @@ pub struct BucketStats {
 }
 
 /// Solve the bucket problem
-pub fn solve(capacity_1: u8, capacity_2: u8, goal: u8, start_bucket: &Bucket) -> Option<BucketStats> {
-    unimplemented!(
-        "Given one bucket of capacity {}, another of capacity {}, starting with {:?}, find pours to reach {}, or None if impossible",
-        capacity_1,
-        capacity_2,
-        start_bucket,
-        goal,
-    );
+pub fn solve(
+    capacity_1: u8,
+    capacity_2: u8,
+    goal: u8,
+    start_bucket: &Bucket,
+) -> Option<BucketStats> {
+    let mut tried_already = HashSet::new();
+    match start_bucket {
+        Bucket::One => tried_already.insert((0, capacity_2)),
+        Bucket::Two => tried_already.insert((capacity_1, 0)),
+    };
+
+    let mut queue = VecDeque::from(vec![(0, 0, 0)]);
+
+    while let Some((moves, bucket_1, bucket_2)) = queue.pop_front() {
+        if tried_already.contains(&(bucket_1, bucket_2)) {
+            continue;
+        }
+        tried_already.insert((bucket_1, bucket_2));
+        if bucket_1 == goal {
+            return Some(BucketStats {
+                moves: moves,
+                goal_bucket: Bucket::One,
+                other_bucket: bucket_2,
+            });
+        } else if bucket_2 == goal {
+            return Some(BucketStats {
+                moves: moves,
+                goal_bucket: Bucket::Two,
+                other_bucket: bucket_1,
+            });
+        }
+        queue.push_back((moves + 1, 0, bucket_2));
+        queue.push_back((moves + 1, bucket_1, 0));
+        queue.push_back((moves + 1, capacity_1, bucket_2));
+        queue.push_back((moves + 1, bucket_1, capacity_2));
+        let volume = bucket_1 + bucket_2;
+        if volume > capacity_2 {
+            queue.push_back((moves + 1, volume - capacity_2, capacity_2));
+        } else {
+            queue.push_back((moves + 1, 0, volume));
+        }
+        if volume > capacity_1 {
+            queue.push_back((moves + 1, capacity_1, volume - capacity_1));
+        } else {
+            queue.push_back((moves + 1, volume, 0));
+        }
+    }
+    None
 }

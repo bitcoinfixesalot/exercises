506e543022b8f5f400eb504dddca1f41fb89174d
Svetlan <s.atanasov@live.com>
2019-10-29 21:46:34 +0000

    spiral-matrix

--- spiral-matrix/src/lib.rs
+++ spiral-matrix/src/lib.rs
@@ -1,6 +1,51 @@
+enum Direction {
+    Right,
+    Left,
+    Down,
+    Up,
+}
+
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
-    unimplemented!(
-        "Function that returns the spiral matrix of square size {}",
-        size
-    );
+    let mut matrix = vec![vec![0; size as usize]; size as usize];
+    let mut direction = Direction::Right;
+    let mut col: i32 = 0;
+    let mut row: i32 = 0;
+
+    for i in 1..((size * size) + 1) {
+        match direction {
+            Direction::Right
+                if col as u32 > size - 1 || matrix[row as usize][col as usize] != 0 =>
+            {
+                direction = Direction::Down;
+                col -= 1;
+                row += 1;
+            }
+            Direction::Down if row as u32 > size - 1 || matrix[row as usize][col as usize] != 0 => {
+                direction = Direction::Left;
+                col -= 1;
+                row -= 1;
+            }
+            Direction::Left if col < 0 || matrix[row as usize][col as usize] != 0 => {
+                direction = Direction::Up;
+                col += 1;
+                row -= 1;
+            }
+            Direction::Up if row < 0 || matrix[row as usize][col as usize] != 0 => {
+                direction = Direction::Right;
+                col += 1;
+                row += 1;
+            }
+            _ => (),
+        }
+        matrix[row as usize][col as usize] = i;
+
+        match direction {
+            Direction::Right => col += 1,
+            Direction::Down => row += 1,
+            Direction::Left => col -= 1,
+            Direction::Up => row -= 1,
+        }
+    }
+
+    matrix
 }
--- spiral-matrix/tests/spiral-matrix.rs
+++ spiral-matrix/tests/spiral-matrix.rs
@@ -1,6 +1,7 @@
 use spiral_matrix::*;
 
 #[test]
+#[ignore]
 fn empty_spiral() {
     let expected: Vec<Vec<u32>> = Vec::new();
     assert_eq!(spiral_matrix(0), expected);
@@ -32,7 +33,6 @@ fn size_three_spiral() {
     assert_eq!(spiral_matrix(3), expected);
 }
 #[test]
-#[ignore]
 fn size_four_spiral() {
     let mut expected: Vec<Vec<u32>> = Vec::new();
     expected.push(vec![1, 2, 3, 4]);

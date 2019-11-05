9d971307a63c20b9a201e52209ad4a00a6f6d395
Svetlan <s.atanasov@live.com>
2019-11-05 21:42:08 +0000

    sublist

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -6,6 +6,19 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    if first_list.len() == second_list.len() && is_sub(first_list, second_list) {
+        return Comparison::Equal;
+    }
+    if first_list.len() < second_list.len() && is_sub(first_list, second_list) {
+        return Comparison::Sublist;
+    }
+    if first_list.len() > second_list.len() && is_sub(second_list, second_list) {
+        return Comparison::Superlist;
+    }
+    Comparison::Unequal
+}
+
+fn is_sub<T: PartialEq>(short: &[T], long: &[T]) -> bool {
+    short.is_empty() || long.windows(short.len()).any(|l| l == short)
 }

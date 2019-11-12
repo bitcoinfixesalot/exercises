bff047aff1d2c28fba9794503027549f82ff6e94
Svetlan <s.atanasov@live.com>
2019-11-12 21:35:55 +0000

    saddle-points

--- saddle-points/src/lib.rs
+++ saddle-points/src/lib.rs
@@ -1,6 +1,13 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    let mut saddle_points: Vec<(usize, usize)> = Vec::new();
+
+    for (row, row_vector) in input.iter().enumerate() {
+        for (col, number) in row_vector.iter().enumerate() {
+            if row_vector.iter().all(|a| a <= number) && input.iter().all(|a| a[col] >= *number) {
+                saddle_points.push((row, col));
+            }
+        }
+    }
+
+    saddle_points
 }

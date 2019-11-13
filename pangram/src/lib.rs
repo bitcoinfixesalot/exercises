98212f5849655443930c0f88d212b307b97deffa
Svetlan <s.atanasov@live.com>
2019-11-13 22:22:56 +0000

    simplifying pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,25 +1,11 @@
 use std::collections::HashSet;
 
 pub fn is_pangram(sentence: &str) -> bool {
-    if sentence.chars().any(|c| !c.is_ascii()) {
-        is_pangram_german(sentence)
-    } else {
-        sentence
-            .chars()
-            .filter(|c| c.is_alphabetic())
-            .collect::<HashSet<char>>()
-            .len()
-            == 26
-    }
-}
-
-fn is_pangram_german(sentence: &str) -> bool {
-    let mut german_alphabet = (b'a'..=b'z').map(char::from).collect::<HashSet<char>>();
-    german_alphabet.extend(&['ä', 'ö', 'ü', 'ß']);
-    german_alphabet
-        == sentence
-            .to_lowercase()
-            .chars()
-            .filter(|c| c.is_alphabetic())
-            .collect::<HashSet<char>>()
+    sentence
+        .to_lowercase()
+        .chars()
+        .filter(|x| x.is_ascii_alphabetic())
+        .collect::<HashSet<char>>()
+        .len()
+        == 26
 }

b6eae58cb1a05863cf20889dea2f32260be87db2
Svetlan <s.atanasov@live.com>
2020-02-14 16:36:53 +0000

    minesweeper-fmt and removing comments

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -17,35 +17,31 @@ fn mines_count(minefield: &[&str], i: usize, j: usize) -> Option<u8> {
     if &minefield[i][j..(j + 1)] != " " {
         return None;
     }
-    //let mut count = 0;
 
-    // for x in 0..3 {
-    //     for y in 0..3 {
-    //         if i + 1 < x
-    //             || i + 1 - x >= minefield.len()
-    //             || j + 1 < y
-    //             || j + 1 - y >= minefield[i].len()
-    //             || (x == 1 && y == 1)
-    //         {
-    //             continue;
-    //         } else if &minefield[i + 1 - x][(j + 1 - y)..(j + 2 - y)] == "*" {
-    //             count += 1;
-    //         }
-    //     }
-    // }
-
-    let count= neighbours(i as i32, j as i32).iter().filter_map(|&(x, y)| {
-        let row: &str = minefield.get(x as usize)?;
-        row.chars().nth(y as usize)
-    }).map(|c| match c{ '*' => 1, _=> 0,}).sum();
+    let count = neighbours(i as i32, j as i32)
+        .iter()
+        .filter_map(|&(x, y)| {
+            let row: &str = minefield.get(x as usize)?;
+            row.chars().nth(y as usize)
+        })
+        .map(|c| match c {
+            '*' => 1,
+            _ => 0,
+        })
+        .sum();
 
     Some(count)
 }
 
 fn neighbours(x: i32, y: i32) -> [(i32, i32); 8] {
     [
-        (x-1, y-1), (x, y-1), (x+1, y-1),
-        (x-1, y),               (x+1, y),
-        (x-1, y+1), (x, y+1), (x+1, y+1)
+        (x - 1, y - 1),
+        (x, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x - 1, y + 1),
+        (x, y + 1),
+        (x + 1, y + 1),
     ]
 }

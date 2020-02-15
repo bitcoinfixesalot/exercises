17828860addf482b417c50e664133e6305379b44
Svetlan <s.atanasov@live.com>
2020-02-15 22:39:39 +0000

    allergies

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -1,33 +1,55 @@
-pub struct Allergies;
+use self::Allergen::*;
+use std::slice::Iter;
 
-#[derive(Debug, PartialEq)]
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+impl Allergen {
+    pub fn iter() -> Iter<'static, Allergen> {
+        static ALLERGENS: [Allergen; 8] = [
+            Eggs,
+            Peanuts,
+            Shellfish,
+            Strawberries,
+            Tomatoes,
+            Chocolate,
+            Pollen,
+            Cats,
+        ];
+        ALLERGENS.iter()
+    }
 }
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies { score: score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        let score = self.score;
+        let allergen = *allergen as u32;
+        score & allergen != 0
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        Allergen::iter().fold(Vec::new(), |mut vec, elem| {
+            if self.is_allergic_to(&elem) {
+                vec.push(*elem);
+            }
+            vec
+        })
     }
 }

a7eaf1b73105df940ff780769904e648a77f1650
Svetlan <s.atanasov@live.com>
2020-02-15 16:35:45 +0000

    isbn-verifier solution

--- isbn-verifier/src/lib.rs
+++ isbn-verifier/src/lib.rs
@@ -1,4 +1,25 @@
+const X: char = 'X';
+
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {:?} a valid ISBN number?", isbn);
+    let isbn = isbn.chars().filter_map(to_digit).collect::<Vec<u32>>();
+
+    if isbn.len() != 10 || isbn[..9].iter().any(|d| *d == 10) {
+        false
+    } else {
+        isbn.iter()
+            .enumerate()
+            .map(|(i, d)| *d * (10 - i as u32))
+            .sum::<u32>()
+            % 11
+            == 0
+    }
+}
+
+fn to_digit(c: char) -> Option<u32> {
+    match c {
+        _ if c.is_numeric() => Some(c.to_digit(10).unwrap()),
+        X => Some(10),
+        _ => None,
+    }
 }

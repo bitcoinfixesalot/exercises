bce422426fa9696daba6ea9b1fd7b5cdbe5e747b
Svetlan <s.atanasov@live.com>
2020-02-16 14:28:53 +0000

    pig-latin

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,6 +1,23 @@
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{}'",
-        input
-    );
+    input
+        .split(' ')
+        .map(pig_latin)
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+
+fn pig_latin(word: &str) -> String {
+    let mut i = if word.starts_with('y') {
+        word.find(|c| "aeiou".contains(c)).unwrap()
+    } else {
+        word.find(|c| "aeiouy".contains(c)).unwrap()
+    };
+    if i >= 1 && &word[i - 1..=i] == "qu" {
+        i += 1
+    }
+    if &word[..2] == "yt" || "ay" == &word[i..] {
+        i = 0
+    }
+    format!("{}{}ay", &word[i..], &word[..i])
 }

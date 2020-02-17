7149fb1e90793ef6ff74a3145735d59f3d62ae6c
Svetlan <s.atanasov@live.com>
2020-02-17 21:42:27 +0000

    crypto-square

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -1,3 +1,31 @@
 pub fn encrypt(input: &str) -> String {
-    unimplemented!("Encrypt {:?} using a square code", input)
+    let mut input: String = input
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .collect();
+
+    let length = input.len();
+    let (cols, rows) = dimensions(length);
+
+    input.push_str(" ".repeat(rows * cols - length).as_str());
+
+    let mut results = Vec::new();
+
+    for r in 0..rows {
+        results.push(input.chars().skip(r).step_by(rows).collect::<String>());
+    }
+
+    results.join(" ")
+}
+
+fn dimensions(length: usize) -> (usize, usize) {
+    let rows = (length as f64).sqrt() as usize;
+    let mut cols = rows;
+
+    if rows * cols < length {
+        cols += 1;
+    }
+
+    (rows, cols)
 }

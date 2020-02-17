e8f254ad640b6884531650b1140a976a49d1c176
Svetlan <s.atanasov@live.com>
2020-02-17 18:16:26 +0000

    variable-lenght-quantity

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -6,10 +6,49 @@ pub enum Error {
 
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    unimplemented!("Convert the values {:?} to a list of bytes", values)
+    values
+    .iter()
+    .flat_map(|&v| convert_number(v))
+    .collect::<Vec<u8>>()
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    unimplemented!("Convert the list of bytes {:?} to a list of numbers", bytes)
+    let mut values: Vec<u32> = vec![0];
+    let last_byte = bytes.len() - 1;
+    for (i, &byte) in bytes.iter().enumerate() {
+        if let Some(num) = values.last_mut() {
+            if *num >= (std::u32::MAX >> 4) {
+                return Err(Error::Overflow);
+            }
+            *num = (*num << 7) | u32::from(byte & 0x7F);
+        }
+        if byte & 0x80 == 0x00 && i != last_byte {
+            values.push(0);
+        } else if byte & 0x80 != 0x00 && i == last_byte {
+            return Err(Error::IncompleteNumber);
+        }
+    }
+    Ok(values)
+}
+
+
+fn convert_number(n: u32) -> Vec<u8> {
+    if n == 0 {
+        return vec![0];
+    }
+
+    let mut v = n;
+    let mut result: Vec<u8> = Vec::new();
+    while v != 0 {
+        let current = (v % 128) as u8;
+        if result.is_empty() {
+            result.insert(0, current);
+        } else {
+            result.insert(0, current | 0x80);
+        }
+        v = v / 128;
+    }
+
+    result
 }

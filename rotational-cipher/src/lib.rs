bdf75eeb889db5861303fa826dd39af02a759eec
Svetlan <s.atanasov@live.com>
2020-02-18 22:13:07 +0000

    rotational-cipher

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,18 +1,17 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    let key = (key % 26) + if key < 0 { 26 } else { 0 };
-    let key_iter = (0_i8..=26).map(|i| ((i + key) % 26) as u8);
-    let lowercase_key: Vec<char> = key_iter.clone().map(|i| char::from(i + b'a')).collect();
-    let uppercase_key: Vec<char> = key_iter.clone().map(|i| char::from(i + b'A')).collect();
-    input
-        .chars()
-        .map(|c| {
-            if c.is_ascii_alphabetic() && c.is_ascii_lowercase() {
-                lowercase_key[c as usize - b'a' as usize]
-            } else if c.is_ascii_alphabetic() && c.is_ascii_uppercase() {
-                uppercase_key[c as usize - b'A' as usize]
-            } else {
-                c
-            }
-        })
-        .collect()
+    let key = ((key % 26) + if key < 0 { 26 } else { 0 }) as u8;
+
+    String::from_utf8(
+        input
+            .bytes()
+            .map(
+                |c| match (c.is_ascii_alphabetic(), c.is_ascii_lowercase()) {
+                    (true, true) => (c + key - b'a') % 26 + (b'a'),
+                    (true, false) => (c + key - b'A') % 26 + (b'A'),
+                    _ => c,
+                },
+            )
+            .collect(),
+    )
+    .unwrap()
 }

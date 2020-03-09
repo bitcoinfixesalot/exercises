5b363a5563e53ba8d014559896c3dce1cd694b2e
Svetlan <svetlan.atanasov@gmail.com>
2020-03-09 22:02:14 +0000

    removing comments

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -19,29 +19,4 @@ pub fn is_valid(code: &str) -> bool {
         Some((sum, count)) => count > 1 && sum % 10 == 0,
         None => false,
     }
-    // let code = code
-    //     .chars()
-    //     .filter(|c| !c.is_whitespace())
-    //     .collect::<Vec<char>>();
-
-    //     if code.iter().all(|c| c.is_ascii_digit()) && code.len() > 1 {
-    //     let sum = code
-    //         .iter()
-    //         .filter_map(|c| c.to_digit(10))
-    //         .rev()
-    //         .enumerate()
-    //         .fold(0, |acc, (index, n)| {
-    //             if index % 2 == 0 {
-    //                 acc + n
-    //             } else if n * 2 > 9 {
-    //                 acc + n * 2 - 9
-    //             } else {
-    //                 acc + n * 2
-    //             }
-    //         });
-
-    //     sum % 10 == 0
-    // } else {
-    //     false
-    // }
 }

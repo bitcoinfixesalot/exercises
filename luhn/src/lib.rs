81c987b02a1ea9a9b10e9d119ef6bf9df43ac438
Svetlan <svetlan.atanasov@gmail.com>
2020-03-09 14:56:54 +0000

    luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,28 +1,47 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let code = code
+    let result = code
         .chars()
         .filter(|c| !c.is_whitespace())
-        .collect::<Vec<char>>();
-
-        if code.iter().all(|c| c.is_ascii_digit()) && code.len() > 1 {
-        let sum = code
-            .iter()
-            .filter_map(|c| c.to_digit(10))
-            .rev()
-            .enumerate()
-            .fold(0, |acc, (index, n)| {
-                if index % 2 == 0 {
-                    acc + n
-                } else if n * 2 > 9 {
-                    acc + n * 2 - 9
-                } else {
-                    acc + n * 2
+        .rev()
+        .enumerate()
+        .try_fold((0, 0), |(sum, count), (index, c)| {
+            c.to_digit(10).map(|number| {
+                let mut acc = if index % 2 == 1 { number * 2 } else { number };
+                if acc > 9 {
+                    acc -= 9;
                 }
-            });
+                (sum + acc, count + 1)
+            })
+        });
 
-        sum % 10 == 0
-    } else {
-        false
+    match result {
+        Some((sum, count)) => count > 1 && sum % 10 == 0,
+        None => false,
     }
+    // let code = code
+    //     .chars()
+    //     .filter(|c| !c.is_whitespace())
+    //     .collect::<Vec<char>>();
+
+    //     if code.iter().all(|c| c.is_ascii_digit()) && code.len() > 1 {
+    //     let sum = code
+    //         .iter()
+    //         .filter_map(|c| c.to_digit(10))
+    //         .rev()
+    //         .enumerate()
+    //         .fold(0, |acc, (index, n)| {
+    //             if index % 2 == 0 {
+    //                 acc + n
+    //             } else if n * 2 > 9 {
+    //                 acc + n * 2 - 9
+    //             } else {
+    //                 acc + n * 2
+    //             }
+    //         });
+
+    //     sum % 10 == 0
+    // } else {
+    //     false
+    // }
 }

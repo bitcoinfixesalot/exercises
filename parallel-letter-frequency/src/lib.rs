5e46079bb459dc18f0451584679a0e14248cfbd6
Svetlan <svetlan.atanasov@gmail.com>
2020-03-11 14:39:48 +0000

    parallel-letter-frequency

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -5,22 +5,21 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     (0..worker_count)
         .into_par_iter()
         .map(|i| {
-            let mut letters = HashMap::new();
-            input
-                .join("")
+            let mut letters_count = HashMap::new();
+            format!("{:?}", input)
                 .chars()
                 .skip(i)
                 .step_by(worker_count)
                 .filter(|c| c.is_alphabetic())
                 .flat_map(|c| c.to_lowercase())
                 .for_each(|c| {
-                    *letters.entry(c).or_insert(0) += 1;
+                    *letters_count.entry(c).or_insert(0) += 1;
                 });
-            letters
+            letters_count
         })
-        .reduce(HashMap::new, |mut result, m| {
-            m.iter().for_each(|(&k, &v)| {
-                *result.entry(k).or_insert(0) += v;
+        .reduce(HashMap::new, |mut result, hmap| {
+            hmap.iter().for_each(|(&key, &value)| {
+                *result.entry(key).or_insert(0) += value;
             });
             result
         })

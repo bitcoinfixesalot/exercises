30c5da100a013a1d07c92ffa81b60600ccb9c43a
Svetlan <svetlan.atanasov@gmail.com>
2020-03-11 13:52:38 +0000

    parallel-letter-frequency

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,26 @@
 use std::collections::HashMap;
+use rayon::prelude::*;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    unimplemented!(
-        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
-        input,
-        match worker_count {
-            1 => format!("1 worker"),
-            _ => format!("{} workers", worker_count),
-        }
-    );
+    (0..worker_count)
+    .into_par_iter()
+    .map(|i| {
+        let mut letters = HashMap::new();
+        input.join("")
+            .chars()
+            .skip(i)
+            .step_by(worker_count)
+            .filter(|c| c.is_alphabetic())
+            .flat_map(|c| c.to_lowercase())
+            .for_each(|c| {
+                *letters.entry(c).or_insert(0) += 1;
+            });
+        letters
+    })
+    .reduce(HashMap::new, |mut result, m| {
+        m.iter().for_each(|(&k, &v)| {
+            *result.entry(k).or_insert(0) += v;
+        });
+        result
+    })
 }

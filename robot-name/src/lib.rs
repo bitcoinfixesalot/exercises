95947395e1771184a9903205615654e53e4836ec
Svetlan <s.atanasov@live.com>
2020-03-22 20:21:55 +0000

    robot-name

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,15 +1,54 @@
-pub struct Robot;
+#[macro_use]
+extern crate lazy_static;
+use rand::Rng;
+use std::collections::HashSet;
+use std::sync::Mutex;
+
+const MAX_TRIALS: usize = 100;
+
+lazy_static! {
+    static ref USED: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
+}
+pub struct Robot {
+    name: String,
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Robot {
+            name: unique_random_name(),
+        }
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        &self.name
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        self.name = unique_random_name();
     }
+}
+
+fn unique_random_name() -> String {
+    for _ in 0..MAX_TRIALS {
+        let name: String = random_name();
+        let mut used = USED.lock().unwrap();
+        if !used.contains(&name) {
+            used.insert(name.clone());
+            return name;
+        }
+    }
+
+    panic!("Unable to find an unique name after {} trials.", MAX_TRIALS);
+}
+
+fn random_name() -> String {
+    let mut rng1 = rand::thread_rng();
+    let mut rng2 = rand::thread_rng();
+    (0..2)
+        .map(|_| rng1.gen_range(0, 26))
+        .map(|c| c + 'A' as u8)
+        .chain((0..3).map(|_| rng2.gen_range(0, 10) + '0' as u8))
+        .map(|c| c as char)
+        .collect()
 }

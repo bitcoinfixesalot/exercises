30da843d73dd1a0d2620391959bfd3bd0700e756
Svetlan <s.atanasov@live.com>
2020-03-26 20:16:03 +0000

    custom-set

--- custom-set/src/lib.rs
+++ custom-set/src/lib.rs
@@ -1,48 +1,65 @@
-use std::marker::PhantomData;
-
 #[derive(Debug, PartialEq)]
 pub struct CustomSet<T> {
-    // This field is here to make the template compile and not to
-    // complain about unused type parameter 'T'. Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
-    phantom: PhantomData<T>,
+    set: Vec<T>,
 }
 
-impl<T> CustomSet<T> {
-    pub fn new(_input: &[T]) -> Self {
-        unimplemented!();
+impl<T: Clone + PartialEq + Ord> CustomSet<T> {
+    pub fn new(input: &[T]) -> Self {
+        let mut custom_set = CustomSet {
+            set: input.to_vec(),
+        };
+        custom_set.set.sort();
+        custom_set
     }
 
-    pub fn contains(&self, _element: &T) -> bool {
-        unimplemented!();
+    pub fn contains(&self, element: &T) -> bool {
+        self.set.contains(element)
     }
 
-    pub fn add(&mut self, _element: T) {
-        unimplemented!();
+    pub fn add(&mut self, element: T) {
+        if !self.set.contains(&element) {
+            self.set.push(element);
+            self.set.sort();
+        }
     }
 
-    pub fn is_subset(&self, _other: &Self) -> bool {
-        unimplemented!();
+    pub fn is_subset(&self, other: &Self) -> bool {
+        self.set.iter().all(|element| other.contains(element))
     }
 
     pub fn is_empty(&self) -> bool {
-        unimplemented!();
+        self.set.is_empty()
     }
 
-    pub fn is_disjoint(&self, _other: &Self) -> bool {
-        unimplemented!();
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        !self.set.iter().any(|element| other.contains(element))
     }
 
-    pub fn intersection(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn intersection(&self, other: &Self) -> Self {
+        let mut set = CustomSet { set: Vec::new() };
+        for element in self.set.iter() {
+            if other.contains(element) {
+                set.add(element.clone());
+            }
+        }
+        set
     }
 
-    pub fn difference(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn difference(&self, other: &Self) -> Self {
+        let mut set = CustomSet { set: Vec::new() };
+        for element in self.set.iter() {
+            if !other.contains(element) {
+                set.add(element.clone());
+            }
+        }
+        set
     }
 
-    pub fn union(&self, _other: &Self) -> Self {
-        unimplemented!();
+    pub fn union(&self, other: &Self) -> Self {
+        let mut set = CustomSet { set: Vec::new() };
+        for element in self.set.iter().chain(other.set.iter()) {
+            set.add(element.clone());
+        }
+        set
     }
 }

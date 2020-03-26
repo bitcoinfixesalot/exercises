884d2e6b4b6d2f0b6a1d170d2d6e0f3ccc1fd14a
Svetlan <s.atanasov@live.com>
2020-03-26 17:39:26 +0000

    diffie-hellman

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,21 +1,28 @@
+use rand::Rng;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    rand::thread_rng().gen_range(2, p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    let mut a = a;
+    let mut g = g;
+    if a == 1 {
+        return 0;
+    }
+
+    let mut result = 1;
+
+    while a > 0 {
+        if a % 2 == 1 {
+            result = (result * g) % p;
+        }
+        a >>= 1;
+        g = g.pow(2) % p;
+    }
+    result
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    public_key(p, b_pub, a)
 }

4a13535de4c16d27a48f65b78daf31bca05ce5d1
Svetlan <s.atanasov@live.com>
2020-03-27 16:26:06 +0000

    diamond

--- diamond/src/lib.rs
+++ diamond/src/lib.rs
@@ -1,6 +1,23 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    unimplemented!(
-        "Return the vector of strings which represent the diamond with particular char {}",
-        c
-    );
+    let mut lines = Vec::new();
+
+    let mut current_letter = 'A';
+
+    while current_letter <= c {
+        let skip = " ".repeat(c as usize - current_letter as usize);
+
+        if current_letter == 'A' {
+            lines.push(format!("{}{}{}", skip, current_letter, skip));
+        } else {
+            let middle = " ".repeat(2 * (current_letter as usize - 'A' as usize) - 1);
+            lines.push(format!("{}{}{}{}{}", skip, current_letter, middle, current_letter, skip));
+        }
+
+        current_letter = (current_letter as u8 + 1) as char;
+    }
+
+    lines.iter()
+        .chain(lines.iter().rev().skip(1))
+        .map(|s| s.to_string())
+        .collect()
 }

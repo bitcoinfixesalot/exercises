5a5529a63938b6b5267b0c114c5a3ba0fcf6d135
Svetlan <s.atanasov@live.com>
2020-03-27 17:59:01 +0000

    phone-number

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,6 +1,21 @@
+const ZERO: char = '0';
+const ONE: char = '1';
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let mut user_number: Vec<char> = user_number.chars().filter(|c| c.is_numeric()).collect();
+
+    if user_number.len() == 11 && user_number[0] == ONE {
+        user_number.remove(0);
+    }
+
+    match user_number.len() {
+        10 if user_number[0] == ZERO
+            || user_number[0] == ONE
+            || user_number[3] == ZERO
+            || user_number[3] == ONE =>
+        {
+            None
+        }
+        10 => Some(user_number.iter().collect()),
+        _ => None,
+    }
 }

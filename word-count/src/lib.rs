dd64df9b47c23f7123f37ec3ed87afcd6425cf39
Svetlan <s.atanasov@live.com>
2020-03-28 18:56:36 +0000

    word-count

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
 
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    let mut counts: HashMap<String, u32> = HashMap::new();
-    for word in words
+    let words = words
+        .to_lowercase()
         .split(|c: char| c.is_whitespace() || c == ',')
-        .map(|s| s.trim_matches(|c: char| !c.is_alphanumeric()))
+        .map(|s| s.trim_matches(|c: char| !c.is_alphanumeric()).to_string())
         .filter(|s| !s.is_empty())
-        .map(|s| s.to_lowercase().to_string())
-    {
-        counts.entry(word).and_modify(|v| *v += 1).or_insert(1);
+        .collect::<Vec<String>>();
+
+    let mut result: HashMap<String, u32> = HashMap::new();
+
+    for word in words {
+        result.entry(word).and_modify(|v| *v += 1).or_insert(1);
     }
-    counts
+
+    result
 }

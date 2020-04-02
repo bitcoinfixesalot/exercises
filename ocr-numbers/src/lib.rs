f2fc2570f36c40963ff5a1efe8d8dc2953574ee0
Svetlan <s.atanasov@live.com>
2020-04-02 11:52:34 +0100

    ocr-numbers

--- ocr-numbers/src/lib.rs
+++ ocr-numbers/src/lib.rs
@@ -1,5 +1,4 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -8,5 +7,41 @@ pub enum Error {
 }
 
 pub fn convert(input: &str) -> Result<String, Error> {
-    unimplemented!("Convert the input '{}' to a string", input);
+    let rows = input.lines().collect::<Vec<&str>>();
+    if rows.len() % 4 != 0 {
+        return Err(Error::InvalidRowCount(rows.len()));
+    } else if rows.iter().any(|row| row.len() % 3 != 0) {
+        return Err(Error::InvalidColumnCount(rows[0].len()));
+    }
+
+    let letters = vec![
+        (" _ | ||_|   ", "0"),
+        ("     |  |   ", "1"),
+        (" _  _||_    ", "2"),
+        (" _  _| _|   ", "3"),
+        ("   |_|  |   ", "4"),
+        (" _ |_  _|   ", "5"),
+        (" _ |_ |_|   ", "6"),
+        (" _   |  |   ", "7"),
+        (" _ |_||_|   ", "8"),
+        (" _ |_| _|   ", "9"),
+    ]
+    .iter()
+    .cloned()
+    .collect::<HashMap<&str, &str>>();
+
+    let mut result = String::new();
+    for letter_row in rows.chunks(4) {
+        for col in (0..rows[0].len()).step_by(3) {
+            let letter: String = letter_row
+                .iter()
+                .flat_map(|row| row[col..col + 3].chars())
+                .collect();
+            result += letters.get(letter.as_str()).unwrap_or(&"?");
+        }
+        result.push(',');
+    }
+    result.pop();
+
+    Ok(result)
 }

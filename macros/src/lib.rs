ad6e42a2a032bd941e0cddee240101b2e610401a
Svetlan <s.atanasov@live.com>
2020-04-27 11:46:41 +0100

    macros change

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! hashmap {
     ($($key:expr => $value:expr,)+) => {hashmap!($($key => $value),+)};
-    ($($key:expr => $value:expr),*) => {
+    ($($key:expr => $value:expr),* $(,)?) => {
         {
-            let mut hashmap = std::collections::HashMap::new();
+            let mut hashmap = ::std::collections::HashMap::new();
             $(
                 hashmap.insert($key, $value);
             )*

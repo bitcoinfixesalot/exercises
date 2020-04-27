d9342a8cb20bf867c7938b2b0356a8d03fad8e5f
Svetlan <s.atanasov@live.com>
2020-04-27 11:52:01 +0100

    macros optimization

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,5 @@
 #[macro_export]
 macro_rules! hashmap {
-    ($($key:expr => $value:expr,)+) => {hashmap!($($key => $value),+)};
     ($($key:expr => $value:expr),* $(,)?) => {
         {
             let mut hashmap = ::std::collections::HashMap::new();
